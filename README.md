477e74527d6615965e9547f62bf8b6c8f120cc6e
Joel Donahue <github@joeldonahue.com>
2022-02-03 07:47:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Simple Python Application for YugabyteDB
+
+This application connects to your YugabyteDB instance via the 
+[Python psycopg2 driver](https://docs.yugabyte.com/latest/reference/drivers/ysql-client-drivers/#psycopg2) and performs basic SQL 
+operations. The instructions below are provided for [Yugabyte Cloud](https://cloud.yugabyte.com/) deployments. 
+If you use a different type of deployment, then update the `sample-app.go` file with proper connection parameters.
+
+## Prerequisite
+* Python 3.6 or later is preferred.
+* psycopg2
+
+## Start Yugabyte Cloud Cluster
+
+* [Start YugabyteDB Cloud](https://docs.yugabyte.com/latest/yugabyte-cloud/cloud-quickstart/qs-add/) instance. You can use
+the free tier at no cost.
+* Add an IP address of your machine/laptop to the [IP allow list](https://docs.yugabyte.com/latest/yugabyte-cloud/cloud-secure-clusters/add-connections/#manage-ip-allow-lists)
+
+## Clone Application Repository
+
+Clone the repository and change dirs into it:
+
+```bash
+git clone https://github.com/yugabyte/yugabyte-simple-python-app.git && cd yugabyte-simple-python-app
+```
+
+## Install required packages
+
+```bash
+pip install -r requirements.txt
+```
+
+## Execute the script passing in the required arguments
+Note, you can easily find all the settings on the Yugabyte Cloud dashboard:
+
+![image](resources/cloud_app_settings.png)
+
+```bash
+python3 sample-app.py -u <username> -p <password> -s <hostname> -c </path/to/root.crt>
+```
+
+Upon successful execution, you will see output similar to the following:
+
+```bash
+>>>> Successfully connected to YugabyteDB!
+>>>> Successfully created table DemoAccount.
+>>>> Selecting accounts:
+name = Jessica, age = 28, country = USA, balance = 10000
+name = John, age = 28, country = Canada, balance = 9000
+>>>> Transferred 800 between accounts.
+>>>> Selecting accounts:
+name = Jessica, age = 28, country = USA, balance = 9200
+name = John, age = 28, country = Canada, balance = 9800
+```
+
+## Explore App Logic
+
+Congrats! You've successfully executed a simple Go app that works with Yugabyte Cloud.
+
+Now, explore the source code of `sample-app.go`:
+1. `main` function - establishes a connection with your cloud instance via Go PostgreSQL driver.
+3. `createDatabase` function - creates a table and populates it with sample data.
+4. `selectAccounts` function - queries the data with SQL `SELECT` statements.
+5. `transferMoneyBetweenAccounts` function - updates records consistently with distributed transactions.
+
+## Questions or Issues?
+
+Having issues running this application or want to learn more from Yugabyte experts?
+
+Send a note to [our Slack channel](https://join.slack.com/t/yugabyte-db/shared_invite/zt-xbd652e9-3tN0N7UG0eLpsace4t1d2A),
+or raise a question on StackOverflow and tag the question with `yugabytedb`!
---
+++ sample-app.py
@@ -0,0 +1,92 @@
+import argparse
+import psycopg2
+import psycopg2.extras
+
+
+arg_parser = argparse.ArgumentParser(
+    description="This script connects to a YugabyteDB cluster and performs basic SQL operations.")
+arg_parser.add_argument('-u', '--username', help="The username for connecting to the database.", default="admin")
+arg_parser.add_argument('-p', '--password', help="The password for connecting to the database.")
+arg_parser.add_argument('-s', '--server', help="The server hostname to connect to.")
+arg_parser.add_argument('-c', '--cert_file', help="Full path to the root CA certificate to validate the connection.")
+
+
+def main(args):
+    try:
+        yb = psycopg2.connect(user=args.username, password=args.password,
+                              host=args.server,
+                              port='5433', database='yugabyte',
+                              sslmode="verify-full", sslrootcert=args.cert_file)
+    except Exception as e:
+        print("Exception while connecting to YugabyteDB")
+        print(e)
+        exit(1)
+
+    print(">>>> Successfully connected to YugabyteDB!")
+
+    create_database(yb)
+    select_accounts(yb)
+    transfer_money_between_accounts(yb, 800)
+    select_accounts(yb)
+    yb.close()
+
+
+def create_database(yb):
+    try:
+        with yb.cursor() as yb_cursor:
+            yb_cursor.execute('DROP TABLE IF EXISTS DemoAccount')
+
+            create_table_stmt = """
+                CREATE TABLE DemoAccount (
+                    id int PRIMARY KEY,
+                    name varchar,
+                    age int,
+                    country varchar,
+                    balance int
+                )"""
+            yb_cursor.execute(create_table_stmt)
+
+            insert_stmt = """
+                INSERT INTO DemoAccount VALUES
+                        (1, 'Jessica', 28, 'USA', 10000),
+                        (2, 'John', 28, 'Canada', 9000)"""
+            yb_cursor.execute(insert_stmt)
+        yb.commit()
+    except Exception as e:
+        print("Exception while creating tables")
+        print(e)
+        exit(1)
+
+    print(">>>> Successfully created table DemoAccount.")
+
+
+def select_accounts(yb):
+    print(">>>> Selecting accounts:")
+
+    with yb.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as yb_cursor:
+        yb_cursor.execute("SELECT name, age, country, balance FROM DemoAccount")
+
+        results = yb_cursor.fetchall()
+        for row in results:
+            print("name = {name}, age = {age}, country = {country}, balance = {balance}".format(**row))
+
+
+def transfer_money_between_accounts(yb, amount):
+    try:
+        with yb.cursor() as yb_cursor:
+            yb_cursor.execute("UPDATE DemoAccount SET balance = balance - %s WHERE name = 'Jessica'", [amount])
+            yb_cursor.execute("UPDATE DemoAccount SET balance = balance + %s WHERE name = 'John'", [amount])
+        yb.commit()
+    except (Exception, psycopg2.DatabaseError) as e:
+        print("Exception while transferring money")
+        print(e)
+        if e.pgcode == 40001:
+            print("The operation is aborted due to a concurrent transaction that is modifying the same set of rows." +
+                  "Consider adding retry logic for production-grade applications.")
+        exit(1)
+
+    print(">>>> Transferred {} between accounts.".format(amount))
+
+
+if __name__ == "__main__":
+    main(arg_parser.parse_args())
